6d8dc36984372593b6ce446257beebcef1487710
Maxim Ten <maximten94@gmail.com>
2022-05-24 19:45:38 +0600

    links

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
 import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer';
 import {oldConstitution, newConstitution} from '../constants'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Изменения в конституции 2022</title>
+      </Head>
       <h1>Изменения в конституции 2022</h1>
+      <div className='info'>
+        <a href="http://law.gov.kz/client/#!/doc/1/rus">Старая версия</a>
+        <a href="https://kazpravda.kz/n/proekt-zakon-rk-o-vnesenii-izmeneniy-i-dopolneniy-v-konstitutsiyu-respubliki-kazahstan/">Правки</a>
+      </div>
       <ReactDiffViewer 
         oldValue={oldConstitution} 
         newValue={newConstitution} 
